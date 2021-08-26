ea37086d7fd998e407dbb4e988f4b2a25af68c2e
Laercio Santos <laerciosantosn@gmail.com>
2021-08-26 20:43:09 -0300

    Feat: login and signup with firebase auth

--- pages/signup.js
+++ pages/signup.js
@@ -11,6 +11,8 @@ import {
   FormControl,
   FormLabel,
   FormHelperText,
+  InputLeftAddon,
+  InputGroup,
 } from '@chakra-ui/react'
 
 import { Logo } from '../components'
@@ -19,6 +21,7 @@ import firebase from '../config/firebase'
 const validationSchema = yup.object().shape({
   email: yup.string().email('Invalid email').required('Mandatory filling'),
   password: yup.string().required('Mandatory filling'),
+  username: yup.string().required('Mandatory filling')
 })
 
 export default function Home() {
@@ -32,7 +35,7 @@ export default function Home() {
   } = useFormik({
     onSubmit: async (values, form) => {
       try {
-        const user = await firebase.auth().signInWithEmailAndPassword(values.email, values.password)
+        const user = await firebase.auth().createUserWithEmailAndPassword(values.email, values.password)
         console.log(user)
       } catch (error) {
         console.log('ERROR ', error)
@@ -68,14 +71,23 @@ export default function Home() {
           {touched.password && <FormHelperText textColor="#e74c3c">{errors.password}</FormHelperText>}
         </FormControl>
 
+
+        <FormControl id="username" p={4} isRequired>
+          <InputGroup size="lg">
+            <InputLeftAddon children="clocker.work" />
+            <Input type="username" value={values.username} onChange={handleChange} onBlur={handleBlur} />
+          </InputGroup>
+          {touched.username && <FormHelperText textColor="#e74c3c">{errors.username}</FormHelperText>}
+        </FormControl>
+
+
         <Box p={4}>
           <Button colorScheme="blue" width="100%" onClick={handleSubmit} isLoading={isSubmitting}>Enter</Button>
         </Box>
       </Box>
-
       <Box display="flex">
-        <Text textColor="#707A8A" mr={2}>Not have an account yet?</Text>
-        <Text textColor="#2b6cb0"><Link href="/signup">Register now</Link></Text>
+        <Text textColor="#707A8A" mr={2}>Already registered?</Text>
+        <Text textColor="#2b6cb0"><Link href="/">Log In</Link></Text>
       </Box>
     </Container>
   )
